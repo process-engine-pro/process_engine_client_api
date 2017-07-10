a7e663f8f4fa8529cb90c5e804ba998574245937
Simon Reymann <simon.reymann@gmail.com>
2017-07-10 14:43:16 +0200

    fix task setup when receiving message

--- dist/interfaces.d.ts
+++ dist/interfaces.d.ts
@@ -1,7 +1,6 @@
 import { ExecutionContext } from '@process-engine-js/core_contracts';
 import { IMessage } from '@process-engine-js/messagebus_contracts';
 import { INodeDefEntity, IUserTaskEntity } from '@process-engine-js/process_engine_contracts';
-
 export interface IProcessable {
     handleUserTask(processKey: string, message: IMessage): void;
     handleManualTask(processKey: string, message: IMessage): void;
