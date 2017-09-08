f5dcc19be76c5e928f2dab1d2aae26ec1fee1a57
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 17:24:04 +0200

    :fire: remove dist-folder

--- dist/commonjs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./process_engine_client_api"));
-__export(require("./process_instance"));
-
-//# sourceMappingURL=index.js.map
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './process_engine_client_api';
-export * from './process_instance';
-export * from './interfaces';
